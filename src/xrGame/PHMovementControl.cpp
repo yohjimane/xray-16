d3c8c587328893887ec3b443572340431761b588
dir322 <botkiller4@yandex.ru>
2020-05-29 02:17:28 +0300

    [xrGame] Коллизия актора как в сингле

--- src/xrGame/PHMovementControl.cpp
+++ src/xrGame/PHMovementControl.cpp
@@ -1033,7 +1033,7 @@ void CPHMovementControl::AllocateCharacterObject(CharacterType type)
     switch (type)
     {
     case actor:
-        m_character = create_actor_character(IsGameTypeSingle());
+        m_character = create_actor_character(true);
         break;
     // case actor:	m_character = new CPHActorCharacter	()					;	break;
     // case ai:		m_character = new CPHAICharacter	()					;	break;
