00775560d9b885366ed7fa88ff497167666c4a95
Pavel Spitsin <pspitsin@tamashi.games>
2020-09-14 16:24:03 +0300

    [xrGame] некоторые улучшения по интерполяции мутантов и сталкеров

--- src/xrGame/ai/monsters/basemonster/base_monster_net.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster_net.cpp
@@ -148,10 +148,15 @@ void CBaseMonster::net_Import(NET_Packet& P)
 
         P.r_u8(phSyncFlag);
 
-        if (phSyncFlag)
-            physics_state.read(P);
-        else
-            P.r_vec3(fv_position);
+		if (phSyncFlag)
+		{
+			physics_state.read(P);
+			fv_position.set(physics_state.physics_position);
+		}
+		else
+		{
+			P.r_vec3(fv_position);
+		}
 
         P.r_float(f_health);
         P.r_float(fv_direction.pitch);
@@ -160,6 +165,9 @@ void CBaseMonster::net_Import(NET_Packet& P)
         P.r_u16(u_motion_slot);
 
         SetfHealth(f_health);
+
+        setVisible(TRUE);
+        setEnabled(TRUE);
 
         if (phSyncFlag)
         {
@@ -175,8 +183,22 @@ void CBaseMonster::net_Import(NET_Packet& P)
         }
         else
         {
-            Position().set(fv_position);
-            //TODO: disable interpolation?
+            Fmatrix M;
+            M.setHPB(0.0f, -fv_direction.pitch, 0.0f);
+            XFORM().mulB_43(M);
+            XFORM().rotateY(fv_direction.yaw);
+
+            // pavel: should be after rotation
+            XFORM().translate_over(fv_position);
+            Position().set(fv_position); // we need it?
+
+            // pavel: not tested. We need it?
+            if (!g_Alive())
+            {
+                PHUnFreeze();
+            }
+
+            NET_A.clear();
         }
 
         ApplyAnimation(u_motion_idx, u_motion_slot);
@@ -188,6 +210,11 @@ void CBaseMonster::net_Import(NET_Packet& P)
 
 void CBaseMonster::postprocess_packet(monster_interpolation::net_update_A& N_A)
 {
+    if (!NET_A.empty())
+        N_A.dwTimeStamp = NET_A.back().dwTimeStamp;
+    else
+        N_A.dwTimeStamp = Level().timeServer();
+
     N_A.State.previous_position = N_A.State.position;
     N_A.State.previous_quaternion = N_A.State.quaternion;
 
@@ -484,6 +511,8 @@ void CBaseMonster::make_Interpolation()
             if (m_dwIEndTime != m_dwIStartTime)
                 factor = float(CurTime - m_dwIStartTime) / (m_dwIEndTime - m_dwIStartTime);
 
+            clamp(factor, 0.f, 1.0f);
+
             Fvector NewPos;
             NewPos.lerp(IStart.Pos, IEnd.Pos, factor);
 
@@ -537,8 +566,9 @@ void CBaseMonster::make_Interpolation()
                 R_ASSERT2(0, "Unknown interpolation curve type!");
                 break;
             }
-            Position().set(ResPosition);
-            character_physics_support()->movement()->SetPosition(ResPosition); // we need it ?
+            XFORM().translate_over(ResPosition);
+            Position().set(ResPosition); // we need it?
+            character_physics_support()->movement()->SetPosition(ResPosition); // we need it?
             character_physics_support()->movement()->SetVelocity(SpeedVector);
         };
     }
