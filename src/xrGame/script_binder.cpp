0bf5b24921bea815f77c952ee2c71c895746ec4f
Pavel Spitsin <pspitsin@tamashi.games>
2020-09-18 20:02:05 +0300

    [xrGame] script_binder - включен бинд скриптов для клиента

--- src/xrGame/script_binder.cpp
+++ src/xrGame/script_binder.cpp
@@ -134,18 +134,11 @@ void CScriptBinder::net_Destroy()
 
 void CScriptBinder::set_object(CScriptBinderObject* object)
 {
-    if (OnServer())
-    {
-        VERIFY2(!m_object, "Cannot bind to the object twice!");
+    VERIFY2(!m_object, "Cannot bind to the object twice!");
 #ifdef _DEBUG
-        Msg("* Core object %s is binded with the script object", owner->cName());
+    Msg("* Core object %s is binded with the script object", owner->cName());
 #endif // _DEBUG
-        m_object = object;
-    }
-    else
-    {
-        xr_delete(object);
-    }
+    m_object = object;
 }
 
 void CScriptBinder::shedule_Update(u32 time_delta)
