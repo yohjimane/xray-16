74b076f4493802e870aa1b92be9d8a072c0624ca
yohjimane <nguyen.h.alexander@gmail.com>
2025-03-12 18:01:52 -0700

    * UPD: [xrGame] ui - fix crash in coop mode

--- src/xrGame/ui/UIMMShniaga.cpp
+++ src/xrGame/ui/UIMMShniaga.cpp
@@ -121,7 +121,7 @@ void CUIMMShniaga::InitShniaga(CUIXml& xml_doc, LPCSTR path)
     }
     else
     {
-        if (IsGameTypeSingle())
+        if (IsGameTypeSingle() && !CoopEnabled())
         {
             VERIFY(Actor());
             if (Actor() && !Actor()->g_Alive())
