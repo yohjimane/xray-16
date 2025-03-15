xray-omp/omp-engine

[xrNetServer|xrGame] Фикс обработки сообщений на сервере. Ранее обработка была в потоке стим библиотеки, что не является корректным. Сейчас же перенесено в игровой поток.

src/xrNetServer/BaseServer.h
@@ -48,6 +48,19 @@ struct ClientIdSearchPredicate
 };
 
 // -----------------------------------------------------------------------------
+struct ServerMessage
+{
+	NET_Packet P;
+	ClientID Id;
+
+	ServerMessage(const void* data, u32 data_size, u32 id)
+	{
+		P.construct(data, data_size);
+		Id.set(id);
+	}
+};
+
+// -----------------------------------------------------------------------------
 
 class XRNETSERVER_API BaseServer : public MultipacketReciever
 {
@@ -72,6 +85,9 @@ protected:
 	PlayersMonitor			net_players;
 
 	xrCriticalSection		csMessage;
+	xrCriticalSection				csMessagesQueue;
+
+	xr_deque<ServerMessage>			m_messagesQueue;
 
 	int								  psNET_Port;
 	bool							  m_bDedicated;
@@ -105,6 +121,10 @@ protected:
 
 	virtual void			  _Recieve(const void* data, u32 data_size, u32 param) override;
 	virtual void			  _SendTo_LL(ClientID ID, void* data, u32 size, u32 dwFlags = DPNSEND_GUARANTEED, u32 dwTimeout = 0) = 0;
+
+	// Pavel: Calls from xrServer (from game thread)
+	virtual void              ProcessMessagesQueue();
+
 
 	virtual IClient*		new_client(SClientConnectData* cl_data) = 0;
 

yohjimane/xray-16

src/xrNetServer/BaseServer.h
@@ -6,6 +6,7 @@
 #include "ServerConnectionOptions.h"
 #include "xrCore/buffer_vector.h"
 #include "IClient.h"
+#include "xrCommon/xr_deque.h"
 
 class CServerInfo;
 class IBannedClient;
@@ -35,6 +36,17 @@ public:
 class XRNETSERVER_API BaseServer : public MultipacketReciever
 {
 public:
+	struct ServerMessage
+	{
+		NET_Packet P;
+		ClientID Id;
+
+		ServerMessage(const void* data, u32 data_size, u32 id)
+		{
+			P.construct(data, data_size);
+			Id.set(id);
+		}
+	};
 
     struct ClientIdSearchPredicate
     {
@@ -129,6 +141,8 @@ protected:
 	PlayersMonitor net_players;
 
     Lock csMessage;
+	Lock csMessagesQueue;
+	xr_deque<ServerMessage> m_messagesQueue;
 
 	int psNET_Port;
 	bool m_bDedicated;
@@ -162,7 +176,8 @@ protected:
 
 	virtual void _Recieve(const void* data, u32 data_size, u32 param) override;
 	virtual void _SendTo_LL(ClientID ID, void* data, u32 size, u32 dwFlags = DPNSEND_GUARANTEED, u32 dwTimeout = 0) = 0;
-
+	// Pavel: Calls from xrServer (from game thread)
+	virtual void ProcessMessagesQueue();
 	virtual IClient* new_client(SClientConnectData* cl_data) = 0;
 
 public: