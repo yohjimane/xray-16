xray-omp/omp-engine

[xrGame][Sync] Простейшая синхра сталкеров

src/xrServerEntities/xrServer_Objects_ALife_Monsters.cpp
@@ -2123,22 +2123,79 @@ void CSE_ALifeHumanStalker::STATE_Read		(NET_Packet &tNetPacket, u16 size)
 
 void CSE_ALifeHumanStalker::UPDATE_Write	(NET_Packet &tNetPacket)
 {
-	inherited1::UPDATE_Write	(tNetPacket);
-	inherited2::UPDATE_Write	(tNetPacket);
-	tNetPacket.w_stringZ		(m_start_dialog);
+	if (g_pGamePersistent->GameType() == eGameIDSingle)
+	{
+		inherited1::UPDATE_Write(tNetPacket);
+		inherited2::UPDATE_Write(tNetPacket);
+		tNetPacket.w_stringZ(m_start_dialog);
+	}
+	else
+	{
+		tNetPacket.w_float(get_health());
+		tNetPacket.w_vec3(o_Position);
+		tNetPacket.w_angle8(o_torso.pitch);
+		tNetPacket.w_angle8(o_torso.roll);
+		tNetPacket.w_angle8(o_torso.yaw);
+		tNetPacket.w_angle8(f_head_dir_pitch);
+		tNetPacket.w_angle8(f_head_dir_yaw);
+		tNetPacket.w_u16(u_active_slot);
+		tNetPacket.w_u16(u_torso_anm_idx);
+		tNetPacket.w_u8(u_torso_anm_slot);
+		tNetPacket.w_u16(u_legs_anm_idx);
+		tNetPacket.w_u8(u_legs_anm_slot);
+		tNetPacket.w_u16(u_head_anm_idx);
+		tNetPacket.w_u8(u_head_anm_slot);
+		tNetPacket.w_u16(u_script_anm_idx);
+		tNetPacket.w_u8(u_script_anm_slot);
+	}
 }
 
 void CSE_ALifeHumanStalker::UPDATE_Read		(NET_Packet &tNetPacket)
 {
-	inherited1::UPDATE_Read		(tNetPacket);
-	inherited2::UPDATE_Read		(tNetPacket);
-	tNetPacket.r_stringZ		(m_start_dialog);
+	if (g_pGamePersistent->GameType() == eGameIDSingle)
+	{
+		inherited1::UPDATE_Read(tNetPacket);
+		inherited2::UPDATE_Read(tNetPacket);
+		tNetPacket.r_stringZ(m_start_dialog);
+	}
+	else
+	{
+		tNetPacket.r_float(f_health);
+		tNetPacket.r_vec3(o_Position);
+		tNetPacket.r_angle8(o_torso.pitch);
+		tNetPacket.r_angle8(o_torso.roll);
+		tNetPacket.r_angle8(o_torso.yaw);
+		tNetPacket.r_angle8(f_head_dir_pitch);
+		tNetPacket.r_angle8(f_head_dir_yaw);
+		tNetPacket.r_u16(u_active_slot);
+		tNetPacket.r_u16(u_torso_anm_idx);
+		tNetPacket.r_u8(u_torso_anm_slot);
+		tNetPacket.r_u16(u_legs_anm_idx);
+		tNetPacket.r_u8(u_legs_anm_slot);
+		tNetPacket.r_u16(u_head_anm_idx);
+		tNetPacket.r_u8(u_head_anm_slot);
+		tNetPacket.r_u16(u_script_anm_idx);
+		tNetPacket.r_u8(u_script_anm_slot);
+		set_health(f_health);
+	}
 }
 
 void CSE_ALifeHumanStalker::load			(NET_Packet &tNetPacket)
 {
 	inherited1::load			(tNetPacket);
 	inherited2::load			(tNetPacket);
+}
+
+BOOL CSE_ALifeHumanStalker::Net_Relevant()
+{
+	if (g_pGamePersistent->GameType() == eGameIDSingle)
+	{
+		return inherited1::Net_Relevant();
+	}
+	else
+	{
+		return g_Alive();
+	}
 }
 
 #ifndef XRGAME_EXPORTS

yohjimane/xray-16

src/xrServerEntities/xrServer_Objects_ALife_Monsters.cpp
@@ -873,7 +873,7 @@ u32 CSE_ALifeCreatureAbstract::ef_detector_type() const
 void CSE_ALifeCreatureAbstract::on_death(CSE_Abstract* killer)
 {
     //VERIFY(!m_game_death_time);
-    if (GameID() == eGameIDSingle)
+    if (IsGameTypeSingle())
         m_game_death_time = ai().get_alife() ? alife().time_manager().game_time() : Level().GetGameTime();
     else
         m_game_death_time = Level().GetGameTime();
@@ -1689,14 +1689,12 @@ void CSE_ALifeMonsterBase::STATE_Write(NET_Packet& tNetPacket)
 
 void CSE_ALifeMonsterBase::UPDATE_Read(NET_Packet& tNetPacket)
 {
-#ifdef XRGAME_EXPORTS
-    if (GameID() == eGameIDSingle)
+    if (IsGameTypeSingle())
     {
         inherited1::UPDATE_Read(tNetPacket);
         inherited2::UPDATE_Read(tNetPacket);
     }
     else
-#endif
     {
         tNetPacket.r_float(f_health);
         tNetPacket.r_vec3(o_Position);
@@ -1709,14 +1707,12 @@ void CSE_ALifeMonsterBase::UPDATE_Read(NET_Packet& tNetPacket)
 
 void CSE_ALifeMonsterBase::UPDATE_Write(NET_Packet& tNetPacket)
 {
-#ifdef XRGAME_EXPORTS
-    if (GameID() == eGameIDSingle)
+    if (IsGameTypeSingle())
     {
         inherited1::UPDATE_Write(tNetPacket);
         inherited2::UPDATE_Write(tNetPacket);
     }
     else
-#endif
     {
         tNetPacket.w_float(get_health());
         tNetPacket.w_vec3(o_Position);
@@ -1734,13 +1730,11 @@ void CSE_ALifeMonsterBase::load(NET_Packet& tNetPacket)
 
 BOOL CSE_ALifeMonsterBase::Net_Relevant()
 {
-#ifdef XRGAME_EXPORTS
-    if (GameID() == eGameIDSingle)
+    if (IsGameTypeSingle())
     {
         return inherited1::Net_Relevant();
     }
     else
-#endif
     {
         return g_Alive();
     }
@@ -1866,16 +1860,61 @@ void CSE_ALifeHumanStalker::STATE_Read(NET_Packet& tNetPacket, u16 size)
 
 void CSE_ALifeHumanStalker::UPDATE_Write(NET_Packet& tNetPacket)
 {
-    inherited1::UPDATE_Write(tNetPacket);
-    inherited2::UPDATE_Write(tNetPacket);
-    tNetPacket.w_stringZ(m_start_dialog);
+    if (IsGameTypeSingle())
+    {
+        inherited1::UPDATE_Write(tNetPacket);
+        inherited2::UPDATE_Write(tNetPacket);
+        tNetPacket.w_stringZ(m_start_dialog);
+    }
+    else
+    {
+        tNetPacket.w_float(get_health());
+        tNetPacket.w_vec3(o_Position);
+        tNetPacket.w_float(o_torso.pitch);
+        tNetPacket.w_float(o_torso.roll);
+        tNetPacket.w_float(o_torso.yaw);
+        tNetPacket.w_float(f_head_dir_pitch);
+        tNetPacket.w_float(f_head_dir_yaw);
+        tNetPacket.w_u16(u_active_slot);
+        tNetPacket.w_u16(u_torso_anm_idx);
+        tNetPacket.w_u16(u_torso_anm_slot);
+        tNetPacket.w_u16(u_legs_anm_idx);
+        tNetPacket.w_u16(u_legs_anm_slot);
+        tNetPacket.w_u16(u_head_anm_idx);
+        tNetPacket.w_u16(u_head_anm_slot);
+        tNetPacket.w_u16(u_script_anm_idx);
+        tNetPacket.w_u16(u_script_anm_slot);
+    }
 }
 
 void CSE_ALifeHumanStalker::UPDATE_Read(NET_Packet& tNetPacket)
 {
-    inherited1::UPDATE_Read(tNetPacket);
-    inherited2::UPDATE_Read(tNetPacket);
-    tNetPacket.r_stringZ(m_start_dialog);
+    if (IsGameTypeSingle())
+    {
+        inherited1::UPDATE_Read(tNetPacket);
+        inherited2::UPDATE_Read(tNetPacket);
+        tNetPacket.r_stringZ(m_start_dialog);
+    }
+    else
+    {
+        tNetPacket.r_float(f_health);
+        tNetPacket.r_vec3(o_Position);
+        tNetPacket.r_float(o_torso.pitch);
+        tNetPacket.r_float(o_torso.roll);
+        tNetPacket.r_float(o_torso.yaw);
+        tNetPacket.r_float(f_head_dir_pitch);
+        tNetPacket.r_float(f_head_dir_yaw);
+        tNetPacket.r_u16(u_active_slot);
+        tNetPacket.r_u16(u_torso_anm_idx);
+        tNetPacket.r_u16(u_torso_anm_slot);
+        tNetPacket.r_u16(u_legs_anm_idx);
+        tNetPacket.r_u16(u_legs_anm_slot);
+        tNetPacket.r_u16(u_head_anm_idx);
+        tNetPacket.r_u16(u_head_anm_slot);
+        tNetPacket.r_u16(u_script_anm_idx);
+        tNetPacket.r_u16(u_script_anm_slot);
+        set_health(f_health);
+    }
 }
 
 void CSE_ALifeHumanStalker::load(NET_Packet& tNetPacket)
@@ -1891,6 +1930,18 @@ void CSE_ALifeHumanStalker::FillProps(LPCSTR pref, PropItemVec& values)
     inherited2::FillProps(pref, values);
 }
 #endif // #ifndef MASTER_GOLD
+
+BOOL CSE_ALifeHumanStalker::Net_Relevant()
+{
+    if (IsGameTypeSingle())
+    {
+        return inherited1::Net_Relevant();
+    }
+    else
+    {
+        return g_Alive();
+    }
+}
 
 //////////////////////////////////////////////////////////////////////////
 // CSE_ALifeOnlineOfflineGroup