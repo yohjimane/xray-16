xray-omp/omp-engine

[xrGame] реализован рабочий обыск ящиков для мультиплеера

src/xrGame/InventoryBox.cpp
@@ -65,6 +65,15 @@ void CInventoryBox::OnEvent(NET_Packet& P, u16 type)
 
 			itm->H_SetParent	(NULL, dont_create_shell);
 
+			if (!IsGameTypeSingle() && CurrentGameUI())
+			{
+				if (CurrentGameUI()->ActorMenu().GetMenuMode() == mmDeadBodySearch)
+				{
+					if (this == CurrentGameUI()->ActorMenu().GetInvBox())
+						CurrentGameUI()->OnInventoryAction(smart_cast<CInventoryItem*>(itm), GE_OWNERSHIP_REJECT);
+				}
+			}
+
 			if( m_in_use )
 			{
 				CGameObject* GO		= smart_cast<CGameObject*>(itm);

yohjimane/xray-16

src/xrGame/InventoryBox.cpp
@@ -66,6 +66,15 @@ void CInventoryBox::OnEvent(NET_Packet& P, u16 type)
 
         itm->H_SetParent(NULL, dont_create_shell);
 
+        if (!IsGameTypeSingle() && CurrentGameUI())
+        {
+            if (CurrentGameUI()->GetActorMenu().GetMenuMode() == mmDeadBodySearch)
+            {
+                if (this == CurrentGameUI()->GetActorMenu().GetInvBox())
+                    CurrentGameUI()->OnInventoryAction(smart_cast<CInventoryItem*>(itm), GE_OWNERSHIP_REJECT);
+            }
+        }
+
         if (m_in_use)
         {
             CGameObject* GO = smart_cast<CGameObject*>(itm);