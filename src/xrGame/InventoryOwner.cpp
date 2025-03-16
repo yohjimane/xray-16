9985e78b56912d50855c9afbee53ad56df62e962
yohjimane <nguyen.h.alexander@gmail.com>
2025-03-15 17:51:27 -0700

    * FIX: [xrGame] some potential issues in CInventoryOwner

--- src/xrGame/InventoryOwner.cpp
+++ src/xrGame/InventoryOwner.cpp
@@ -106,7 +106,7 @@ bool CInventoryOwner::net_Spawn(CSE_Abstract* DC)
         return FALSE;
     CSE_Abstract* E = (CSE_Abstract*)(DC);
 
-    if ((IsGameTypeSingle() && !CoopEnabled()) || !smart_cast<CSE_ActorMP*>(E))
+    if ((IsGameTypeSingle() && !CoopEnabled()) || (!smart_cast<CSE_ALifeCreatureActor*>(E) && !smart_cast<CSE_ActorMP*>(E)))
     {
         CSE_ALifeTraderAbstract* pTrader = NULL;
         if (E)
@@ -381,7 +381,12 @@ void CInventoryOwner::SetCommunity(CHARACTER_COMMUNITY_INDEX new_community)
         EA->ChangeTeam(CharacterInfo().Community().team(), EA->g_Squad(), EA->g_Group());
     }
 
-    CSE_Abstract* e_entity = ai().alife().objects().object(EA->ID(), false);
+    CSE_Abstract* e_entity;
+    if (IsGameTypeSingle() && !CoopEnabled())
+        e_entity = ai().alife().objects().object(EA->ID(), false);
+    else
+        e_entity = smart_cast<CSE_Abstract*>(Level().Objects.net_Find(EA->ID()));
+
     if (!e_entity)
         return;
 
